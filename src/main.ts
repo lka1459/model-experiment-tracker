4d87d91ce7ecc711b130ff9063325c032a512b1f
lka1459 <lukapustahija337@gmail.com>
2026-06-25 23:45:32 +1000

    Initial form popup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 window.onload=function(){
-
-    const form = document.querySelector('#experiment-form') as HTMLFormElement
+    
+    const form = document.querySelector('#popupOverlay') as HTMLFormElement
     const projectName = document.querySelector('#pname') as HTMLInputElement
     const modelName = document.querySelector('#mname') as HTMLInputElement
     const accuracy = document.querySelector('#accr') as HTMLInputElement
@@ -8,18 +8,16 @@ window.onload=function(){
     const recall = document.querySelector('#recall') as HTMLInputElement
     const f1Score = document.querySelector('#f1') as HTMLInputElement
     const notes = document.querySelector('#notes') as HTMLTextAreaElement
+    const form_close_btn = document.querySelector('.btn-close-form') as HTMLButtonElement
+    const submit_btn = document.querySelector("#submit_btn") as HTMLButtonElement
 
     form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
-
-    console.log(
-        projectName.value,
-        modelName.value,
-        accuracy.value,
-        precision.value,
-        recall.value,
-        f1Score.value,
-        notes.value
-    )
 })
+
+function togglePopup() {
+        form.classList.toggle('show');
+    }
+(window as any).togglePopup = togglePopup
+
 }
